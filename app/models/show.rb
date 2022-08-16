e6129ead656f4ec9d9c88eda88c60a6b3234765e
William Luck <luck.william.h@gmail.com>
2022-08-15 19:06:07 -0500

    commit tree 31be3bdb18e7fb2099a9ee9247c8690275a4cdf7

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        self.maximum('rating')
+    end
+
+    def self.most_popular_show
+        self.order(rating: :desc).first
+    end
+
+    def self.lowest_rating
+        self.minimum('rating')
+    end
+
+    def self.least_popular_show
+        self.order(rating: :asc).first
+    end
+
+    def self.ratings_sum
+        self.sum('rating')
+    end
+
+    def self.popular_shows
+        self.all.where('rating > 5')
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(name: :asc)
+    end
+
+
+
+
+end
